7ce9b4c531c4630b090a1b3eaeae78da24a4e176
= <jacex17@gmail.com>
2019-11-23 07:56:57 -0500

    change for deployment

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68ece5d78e36453e4323eae431284bf0.js"
+  "/precache-manifest.bd06e81cbd13cf5fc00e62a83274056a.js"
 );
 
 self.addEventListener('message', (event) => {
